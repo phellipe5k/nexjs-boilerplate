6965222e09bd9de14d3d240a03528156ad9ee3ec
phellipe5k <luizinhophe@hotmail.com>
2021-06-05 19:37:17 -0300

    boilerplate done

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Main from 'components/Main';
+
+const Home = () => {
+  return (
+    <Main
+      title="Next Js Boilerplate"
+      description="NextJS | Typescript | Storybook | Styled Components | Styled Icons | Jest | React Testing Libraries | Eslint | Prettier | PLOP | PWA"
+      illustrationSrc="https://media2.giphy.com/media/DIabNXDGI6etO/giphy.gif"
+    />
+  );
+};
+
+export default Home;
