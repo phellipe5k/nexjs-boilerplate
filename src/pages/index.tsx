76243668532cf1f78b11f729ac5217b3e3f83c65
phellipe5k <luizinhophe@hotmail.com>
2021-06-05 19:50:26 -0300

    inserting example image to public assets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const Home = () => {
     <Main
       title="Next Js Boilerplate"
       description="NextJS | Typescript | Storybook | Styled Components | Styled Icons | Jest | React Testing Libraries | Eslint | Prettier | PLOP | PWA"
-      illustrationSrc="https://media2.giphy.com/media/DIabNXDGI6etO/giphy.gif"
+      illustrationSrc="/assets/motleyCrue.gif"
     />
   );
 };
